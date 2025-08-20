3d19713d35f46b1935f249e53b978b3be34e58a5
fahim-hossain-0 <fahimhossain735@gmail.com>
2025-08-20 19:34:58 +0600

    about section

--- src/Layout/Components/AboutMe.jsx
+++ src/Layout/Components/AboutMe.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const AboutMe = () => {
-    return (
-        <>
-            <div>
-                <div>
-                    <div>
-                        <h1>h1</h1>
-                        <p></p>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-        </>
-    );
+  return (
+    <>
+      <div className="bg-gradient-to-r from-[#3A2B63] to-[#3A2B63] mt-20 py-12">
+        <div className="container mx-auto flex justify-center items-center bg-gradient-to-r from-[#2C224F] to-[#2C224F] rounded-4xl px-30 py-16">
+          <div className=" text-center ">
+            <h1 className="text-5xl font-medium">About me</h1>
+            <p className="mt-12">
+              Hi, I’m Fahim Hossain, a passionate Full-Stack Web Developer with
+              over 1 years of experience building dynamic, user-friendly, and
+              scalable web applications. I specialize in crafting responsive
+              interfaces with React, Tailwind CSS, and modern frontend tools,
+              while also developing powerful backends using Node.js, Express,
+              and MongoDB. I enjoy working across the entire stack—from
+              designing intuitive user experiences to building secure APIs and
+              managing databases. My focus is on writing clean, maintainable
+              code, solving real-world problems, and continuously learning to
+              stay ahead in the fast-moving tech world. Beyond coding, I love
+              exploring new technologies, improving my skills, and creating
+              impactful projects that make a difference. My ultimate goal is to
+              grow as a developer while helping businesses and individuals bring
+              their ideas to life through the web.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutMe;
-
-
-
-
-
